daf4b059363e0056cc13deeff1e23e4077ff391d
Edwin <edwinadradaruiz99@gmail.com>
2024-04-15 15:08:40 -0500

    Declararion de metodos crear cuestionario y registrar docente en clase Main, TO DO implementar metodos.

--- ASST/src/main/java/co/edu/unicauca/ASST/AsstApplication.java
+++ ASST/src/main/java/co/edu/unicauca/ASST/AsstApplication.java
@@ -1,13 +1,41 @@
 package co.edu.unicauca.ASST;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import co.edu.unicauca.ASST.Repository.ICuestionarioRepository;
+import co.edu.unicauca.ASST.Repository.IDocenteRepository;
 
 @SpringBootApplication
-public class AsstApplication {
+@Transactional
+public class AsstApplication implements CommandLineRunner {
+
+	@Autowired
+	private ICuestionarioRepository cuestionarioRepository;
+
+	@Autowired
+	private IDocenteRepository docenteRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsstApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
+	private void crearCuestionionario() {
+
+	}
+
+	private void registrarDocente() {
+
+	}
+
 }
+
+

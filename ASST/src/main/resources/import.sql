4026c049915b13658eec8fa1ed8b9591de90e4a1
juanbep <jbecapillimue@gmail.com>
2024-04-17 17:22:30 -0500

    import2.0

--- ASST/src/main/resources/import.sql
+++ ASST/src/main/resources/import.sql
@@ -1,8 +1,23 @@
-INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de opción múltiple', 'Opción Múltiple');
-INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de verdadero/falso', 'Verdadero/Falso');
-INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta corta', 'Respuesta Corta');
-INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta larga', 'Respuesta Larga');
+--INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de opción múltiple', 'Opción Múltiple');
+--INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de verdadero/falso', 'Verdadero/Falso');
+--INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta corta', 'Respuesta Corta');
+--INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta larga', 'Respuesta Larga');
 
+
+INSERT INTO departamento (descripcion, nombreDep) 
+VALUES ('Departamento de Matematicas', 'Matematicas');
+
+INSERT INTO departamento (descripcion, nombreDep) 
+VALUES ('Departamento de Informatica', 'Informatica');
+
+INSERT INTO departamento (descripcion, nombreDep) 
+VALUES ('Departamento de Historia', 'Historia');
+
+INSERT INTO departamento (descripcion, nombreDep) 
+VALUES ('Departamento de Fisica', 'Fisica');
+
+INSERT INTO departamento (descripcion, nombreDep) 
+VALUES ('Departamento de Biologia', 'Biologia');
 
 INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Autorizacion de participacion en el Programa de Prevencion de los Factores Psicosociales', 'Autorizacion');
 INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Consentimiento Informado Para Procesos Psicosociales', 'Datos Generales');

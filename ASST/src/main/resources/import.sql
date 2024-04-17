a578e3a59068fbe00a8e5e662111a28e3e0782f8
juanbep <jbecapillimue@gmail.com>
2024-04-17 16:15:09 -0500

    import

---
+++ ASST/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de opción múltiple', 'Opción Múltiple');
+INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de verdadero/falso', 'Verdadero/Falso');
+INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta corta', 'Respuesta Corta');
+INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta larga', 'Respuesta Larga');
+
+
+INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Autorizacion de participacion en el Programa de Prevencion de los Factores Psicosociales', 'Autorizacion');
+INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Consentimiento Informado Para Procesos Psicosociales', 'Datos Generales');
+INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Cuestionario de Factores de Riesgo Psicosocial Intralaboral - Forma A', 'Factores Psicosociales Intralaborales');
+INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Cuestionario de Factores Psicosociales Extralaborales', 'Factores Psicosociales Extralaborales');

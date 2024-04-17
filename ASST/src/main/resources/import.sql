1e3913f42377848d84f7f4432f3e9d96504b6863
juanbep <jbecapillimue@gmail.com>
2024-04-17 17:37:41 -0500

    error de sintaxis corregido

--- ASST/src/main/resources/import.sql
+++ ASST/src/main/resources/import.sql
@@ -4,20 +4,11 @@
 --INSERT INTO `tipopregunta` (`descripcion`, `nombre`) VALUES ('Pregunta de respuesta larga', 'Respuesta Larga');
 
 
-INSERT INTO departamento (descripcion, nombreDep) 
-VALUES ('Departamento de Matematicas', 'Matematicas');
-
-INSERT INTO departamento (descripcion, nombreDep) 
-VALUES ('Departamento de Informatica', 'Informatica');
-
-INSERT INTO departamento (descripcion, nombreDep) 
-VALUES ('Departamento de Historia', 'Historia');
-
-INSERT INTO departamento (descripcion, nombreDep) 
-VALUES ('Departamento de Fisica', 'Fisica');
-
-INSERT INTO departamento (descripcion, nombreDep) 
-VALUES ('Departamento de Biologia', 'Biologia');
+INSERT INTO `departamento` (`descripcion`, `nombreDep`) VALUES ('Departamento de Matematicas', 'Matematicas');
+INSERT INTO `departamento` (`descripcion`, `nombreDep`) VALUES ('Departamento de Informatica', 'Informatica');
+INSERT INTO `departamento` (`descripcion`, `nombreDep`) VALUES ('Departamento de Historia', 'Historia');
+INSERT INTO `departamento` (`descripcion`, `nombreDep`) VALUES ('Departamento de Fisica', 'Fisica');
+INSERT INTO `departamento` (`descripcion`, `nombreDep`) VALUES ('Departamento de Biologia', 'Biologia');
 
 INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Autorizacion de participacion en el Programa de Prevencion de los Factores Psicosociales', 'Autorizacion');
 INSERT INTO `cuestionario` (`descripcion`, `titulo`) VALUES ('Consentimiento Informado Para Procesos Psicosociales', 'Datos Generales');
